19d49f78424df7d75f99eabe170d9b04af9afffb
gulyasm <mgulyas86@gmail.com>
2014-05-07 14:07:49 +0200

    Skeloton file added

---
+++ src/skeloton.js
@@ -0,0 +1,14 @@
+var _SESSIONID;
+if (typeof(SESSIONID) == 'undefined') { _SESSIONID = 'na_SESSIONID'; } else { _SESSIONID = SESSIONID; }
+
+var _meta;
+if (typeof(meta) == 'undefined') { _meta = 'na_SESSIONID'; } else { _meta = meta; }
+
+include "config.js"
+
+(function(SESSIONID, meta, ts, config){
+
+include "body.js"
+
+
+})(SESSIONID, meta, ts, config);
