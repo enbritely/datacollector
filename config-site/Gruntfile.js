d821a74cecf9c288270e68d7f3ae987e95f1b515
gulyasm <mgulyas86@gmail.com>
2014-05-09 17:59:51 +0200

    browserify example done

--- config-site/Gruntfile.js
+++ config-site/Gruntfile.js
@@ -1,34 +1,43 @@
 module.exports = function(grunt) {
   var distFile = 'dist/<%= pkg.name %>.js';
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    clean: ['dist'],
-    copy: {
-     main: {
-      files: [
+        pkg: grunt.file.readJSON('package.json'),
+        clean: ['dist'],
+        copy: {
+         main: {
+          files: [
           {expand: true, flatten: true, src: ['src/**'], dest: 'dist/', filter: 'isFile'},
           {expand: true, src: ['public/**/*'], dest: 'dist/', filter: 'isFile'},
-        ]
-      }
-     },
-     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', '.jshintrc'],
-      options: {
-        reporter: require('jshint-stylish'),
-        force: true,
-        jshintrc: true
-      }
+          ]
+        }
+      },
+      jshint: {
+        files: ['Gruntfile.js', 'src/**/*.js', '.jshintrc'],
+        options: {
+          reporter: require('jshint-stylish'),
+          force: true,
+          jshintrc: true
+        }
 
-    },
-    watch: {
+      },
+      browserify: {
+        client: {
+         src: ['src/**/*.js'],
+         dest: 'dist/client.js',
+         options: {
+         }
+       }
+     },
+     watch: {
       files: ['<%= jshint.files %>', 'src/**/*.html'],
-      tasks: ['clean', 'jshint','copy']
+      tasks: ['clean', 'jshint', 'browserify','copy']
     }
   });
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', ['clear', 'jshint', 'copy']);
+  grunt.registerTask('default', ['clear', 'jshint', 'browserify' ,'copy']);
 };
