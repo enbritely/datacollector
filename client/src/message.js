88dbb684b9a1a159fec80153fb04f87e6f845726
gulyasm <mgulyas86@gmail.com>
2014-08-07 15:46:24 +0200

    Message building refactored.
    Huge refactor. Message building refactored to remove duplicates.
    Now the key and the function is not the same.
    Closes #3

--- client/src/message.js
+++ client/src/message.js
@@ -7,67 +7,61 @@ var Message = function(event, node) {
     var ev = event;
     var no = node;
     var jq_module = require('./jq');
+    var addInteger = function(attr_config, ev, no) {
+        var value = fns[attr_config.func](ev, no);
+        if (!util.isUndefined(value)) {
+            value = Math.round(value);
+        }
+        return value;
+    };
+    var addFloat = function(attr_config, ev, no) {
+        var value = fns[attr_config.func](ev, no);
+        if (!util.isUndefined(value)) {
+            var r = attr_config.zero;
+            value = Math.round(value * Math.pow(10, r)) / Math.pow(10, r);
+        }
+        return value;
+    };
+    var addString = function(attr_config, ev, no) {
+        var value = fns[attr_config.func](ev, no);
+        if (!util.isUndefined(value)) {
+            var trunc = attr_config.validate.max;
+            value = value.substr(0, trunc);
+            var urlencode = attr_config.urlencode;
+            if (urlencode) {
+                value = encodeURIComponent(value);
+            }
+        }
+        return value;
+    };
+    var addAttribute = function(key, attr_config) {
+        var value = typeMap[attr_config.type](attr_config, ev, no);
+        if (util.isUndefined(value)) {
+            value = attr_config["default"];
+        }
+        dict[key] = value;
+    };
+    var getDict = function() {
+        return dict;
+    };
+
+    var typeMap = {
+        "int": addInteger,
+        "string": addString,
+        "float": addFloat
+    }
     return {
-        getDict: function(){
-            return dict;
-        },
+
         // builds the message from the attribute configuration
-        build: function(){
-            for (var k in message_config.attribute_config){
-                this.addAttribute(message_config.attribute_config[k]);
+        build: function() {
+            for (var k in message_config.attribute_config) {
+                addAttribute(k, message_config.attribute_config[k]);
             }
             var sid = event.data.sid;
             return this;
         },
-        // add a generic attribute with an attribute configuration
-        addAttribute: function(attr_config){
-            if (attr_config.type === 'int') {
-                this.addInteger(
-                    attr_config.func,                 // function name
-                    fns[attr_config.func](ev, no),     // value
-                    attr_config['default']                // default
-                    )
-            }
-            if (attr_config.type === 'float') {
-                this.addFloat(attr_config.func, fns[attr_config.func](ev, no), attr_config.zero, attr_config['default'])
-            }
-            if (attr_config.type === 'string') {
-                this.addString(attr_config, ev, no)
-            }
-            if (attr_config.type === 'ts') {
-                this.addTimestamp(attr_config.func, fns[attr_config.func](ev, no), attr_config['default']);
-            }
-        },
-        addInteger: function(key, value, def){
-            dict[key] = util.isUndefined(value) ? def : Math.round(value);
-        },
-        addFloat:   function(key, value, r, def){
-            dict[key] = util.isUndefined(value) ? def : Math.round(value * Math.pow(10, r)) / Math.pow(10, r);
-        },
-        addString:  function(attr_config, ev, no){
-            var key = attr_config.func;
-            var value = fns[attr_config.func](ev, no);
-            var def = attr_config['default'];
-            var trunc = attr_config.validate.max;
-            var urlencode = attr_config.urlencode;
-            var result = util.isUndefined(value) ? def : value.substr(0, trunc);
-            if(urlencode) {
-                result = encodeURIComponent(result);
-            }
-            dict[key] = result;
-        },
-        addTimestamp: function(key, value, def) {
-            if (!util.isUndefined(value)) {
-                dict[key] = def;
-            }
-            else if (value instanceof Date){
-                // TODO
-            }
-            else if (typeof(value) === "string"){
-                // TODO
-            }
-        },
-        send: function(){
+        // Sends the message.
+        send: function() {
             var JSON_str = JSON.stringify(dict);
             var encoded = util.Base64.encode(JSON_str)
             var urlparams = message_config.urlparams;
@@ -75,17 +69,17 @@ var Message = function(event, node) {
             var baseUri = message_config.base.baseUri;
             var path = message_config.base.path;
             var post_url = util.buildQuery(baseUri, path, urlparams);
-            if(message_config.base.test) {
+            if (message_config.base.test) {
                 console.log(post_url);
             } else {
-				var event_timestamp = dict.ts0;
-				if (baseUri !== "") {
-					jq_module.support.cors = true;
-					jq_module.ajax({
-						url: post_url,
-						type: 'GET'
-					}).done(function(data){});
-				}
+                var event_timestamp = dict.ts0;
+                if (baseUri !== "") {
+                    jq_module.support.cors = true;
+                    jq_module.ajax({
+                        url: post_url,
+                        type: 'GET'
+                    }).done(function(data) {});
+                }
             }
         }
     }
