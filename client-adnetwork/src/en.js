08480ea93c869c7dc70fffe2889039bb3679eee7
gulyasm <mgulyas86@gmail.com>
2015-01-12 15:56:34 +0100

    Removed manual minimizations.

--- client-adnetwork/src/en.js
+++ client-adnetwork/src/en.js
@@ -1,18 +1,33 @@
-(function(e,n,b,r,i,te,d,o,t,l,y){
+(function(e, n, b, scriptUrl, wsid, impressionId, partnerId, adId, publisherId, bannerWidth, bannerHeight) {
     var getCookie = function(key) {
         var name = key + "=";
         var ca = document.cookie.split(';');
-        for(var i=0; i < ca.length; i++) {
+        for (var i = 0; i < ca.length; i++) {
             var c = ca[i];
             while (c.charAt(0) === ' ') {
                 c = c.substring(1);
             }
             if (c.indexOf(name) !== -1) {
-                return c.substring(name.length,c.length);
+                return c.substring(name.length, c.length);
             }
         }
         return undefined;
     };
-    e._enbrtly_={'window':e,'document':n,'wsid':i,'imid':te,'pid':d,'aid':o,'hid':t,'banw':l,'banh':y,'sid':getCookie("SESSIONID")};
-    a=n.createElement(b);m=n.getElementsByTagName(b)[0]; a.async=1; a.src=r; m.parentNode.insertBefore(a,m);
-})(window,document,'script','http://2b49fa8f0c16a03e1592-2366b89f86f9049a8d564854bcebe54e.r94.cf5.rackcdn.com/shorte/gerbil.js','shorte','impression_id','partner_id','ad_id','eoptika.hu/','banner_width','banner_height');
+    e._enbrtly_ = {
+        'window': e,
+        'document': n,
+        'wsid': wsid,
+        'imid': impressionId,
+        'pid': partnerId,
+        'aid': adId,
+        'pid': publisherId,
+        'banw': bannerWidth,
+        'banh': bannerHeight,
+        'sid': getCookie("SESSIONID")
+    };
+    a = n.createElement(b);
+    m = n.getElementsByTagName(b)[0];
+    a.async = 1;
+    a.src = scriptUrl;
+    m.parentNode.insertBefore(a, m);
+})(window, document, 'script', 'http://2b49fa8f0c16a03e1592-2366b89f86f9049a8d564854bcebe54e.r94.cf5.rackcdn.com/shorte/gerbil.js', 'shorte', 'impression_id', 'partner_id', 'ad_id', 'eoptika.hu/', 'banner_width', 'banner_height');
