d18b33862a9b5724024e1152b2f041f47d2fc868
hikomat <nagy.gabor.i@gmail.com>
2017-02-07 11:53:26 +0000

    Added Object.assign polyfill. Fixed o.v_current = that.state.viewable.current;

--- gerbil/src/gerbil.js
+++ gerbil/src/gerbil.js
@@ -42,6 +42,33 @@
             };
     }();
     window.requestAnimationFrame(fpsLoop);
+
+    // Object.assign polyfill
+    if (typeof Object.assign != 'function') {
+      Object.assign = function(target, varArgs) { // .length of function is 2
+        'use strict';
+        if (target == null) { // TypeError if undefined or null
+          throw new TypeError('Cannot convert undefined or null to object');
+        }
+
+        var to = Object(target);
+
+        for (var index = 1; index < arguments.length; index++) {
+          var nextSource = arguments[index];
+
+          if (nextSource != null) { // Skip over if undefined or null
+            for (var nextKey in nextSource) {
+              // Avoid bugs when hasOwnProperty is shadowed
+              if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                to[nextKey] = nextSource[nextKey];
+              }
+            }
+          }
+        }
+        return to;
+      };
+    }
+
     // Base64 encoding
     var Base64 = {
         _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
