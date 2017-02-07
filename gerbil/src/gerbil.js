ac9e71d94bec5e3072709393a9656f36fbf21f93
hikomat <nagy.gabor.i@gmail.com>
2017-02-07 13:43:35 +0000

    Added CustomEvent polyfill.

--- gerbil/src/gerbil.js
+++ gerbil/src/gerbil.js
@@ -69,6 +69,44 @@
       };
     }
 
+    try {
+        var ce = new window.CustomEvent('test');
+        ce.preventDefault();
+        if (ce.defaultPrevented !== true) {
+            // IE has problems with .preventDefault() on custom events
+            // http://stackoverflow.com/questions/23349191
+            throw new Error('Could not prevent default');
+        }
+    } catch(e) {
+        var CustomEvent = function(event, params) {
+            var evt, origPrevent;
+            params = params || {
+                bubbles: false,
+                cancelable: false,
+                detail: undefined
+            };
+
+            evt = document.createEvent("CustomEvent");
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+            origPrevent = evt.preventDefault;
+            evt.preventDefault = function () {
+                origPrevent.call(this);
+                try {
+                    Object.defineProperty(this, 'defaultPrevented', {
+                        get: function () {
+                            return true;
+                        }
+                    });
+                } catch(e) {
+                    this.defaultPrevented = true;
+                }
+            };
+            return evt;
+        };
+        CustomEvent.prototype = window.Event.prototype;
+        window.CustomEvent = CustomEvent; // expose definition to window
+    }
+
     // Base64 encoding
     var Base64 = {
         _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
