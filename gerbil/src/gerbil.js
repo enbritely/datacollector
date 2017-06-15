b4bd30c03f1a39c509adf48495029c4695a2b460
Zoltan Hajdu <zoolyka@gmail.com>
2017-06-15 17:30:09 +0200

    Lookupelements added with queryselector polyfill.

--- gerbil/src/gerbil.js
+++ gerbil/src/gerbil.js
@@ -240,6 +240,36 @@
                         }, "") : j;
                     throw new SyntaxError("JSON.parse");
                 });
+
+      // querySelector polyfill
+  
+      if (!document.querySelectorAll) {
+        document.querySelectorAll = function (selectors) {
+          var style = document.createElement('style'), elements = [], element;
+          document.documentElement.firstChild.appendChild(style);
+          document._qsa = [];
+      
+          style.styleSheet.cssText = selectors + '{x-qsa:expression(document._qsa && document._qsa.push(this))}';
+          window.scrollBy(0, 0);
+          style.parentNode.removeChild(style);
+      
+          while (document._qsa.length) {
+            element = document._qsa.shift();
+            element.style.removeAttribute('x-qsa');
+            elements.push(element);
+          }
+          document._qsa = null;
+          return elements;
+        };
+      }
+      
+      if (!document.querySelector) {
+        document.querySelector = function (selectors) {
+          var elements = document.querySelectorAll(selectors);
+          return (elements.length) ? elements[0] : null;
+        };
+      }
+
             }();
 
     /* jshint ignore:end */
@@ -1490,6 +1520,22 @@
         }
     };
 
-    var banner = new EnViewability(null, req);
+    var banner,
+        lookupElementsMaxIterations = 100,
+        lookupElementsInterval = 50;
+
+    var lookupElements = setInterval(function () {
+      if (
+        --lookupElementsMaxIterations < 1
+      ) {
+        clearInterval(lookupElements);
+      } else if (
+        document.querySelectorAll('div.adform-adbox').length > 0 &&
+        document.querySelectorAll('script[adfscript-parsed="true"]').length > 0
+      ) {
+        banner = new EnViewability(null, req);
+        clearInterval(lookupElements);
+      };
+    }, lookupElementsInterval);
 
 }(null));
